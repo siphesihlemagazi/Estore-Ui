a61b7ddf564deb48a9c3b940b18a3eebd6091905
Siphesihle Magazi <siphemagazi@outlook.com>
2023-01-29 16:20:42 +0200

    Added add to cart functionality

--- product_details.js
+++ product_details.js
@@ -7,7 +7,7 @@ fetch(`${apiUrl}/catalogue/product/${productId}/`)
     .then(response => response.json())
     .then(data => {
         const product = document.getElementById('product');
-        console.log(data.images, product)
+        console.log(data)
         product.innerHTML = `
             <div class="col-md-6" id="product-images">
             </div>
@@ -19,45 +19,35 @@ fetch(`${apiUrl}/catalogue/product/${productId}/`)
                         </div> <i class="bi bi-cart text-muted"></i>
                     </div>
                     <div class="mt-4 mb-3"> <span class="text-uppercase text-muted brand">Orianz</span>
-                        <h5 class="text-uppercase">Men's slim fit t-shirt</h5>
+                        <h5 class="text-uppercase">${data.name}</h5>
                         <div class="price d-flex flex-row align-items-center"> <span
                                 class="act-price">$20</span>
                             <div class="ms-2"> <small class="dis-price">$59</small> <span>40% OFF</span>
                             </div>
                         </div>
                     </div>
-                    <p class="about">Shop from a wide range of t-shirt from orianz. Pefect for your
-                        everyday
-                        use, you could pair it with a stylish pair of jeans or trousers complete the
-                        look.
-                    </p>
+                    <p class="about">${data.description}</p>
                     <div class="sizes mt-5">
-                        <h6 class="text-uppercase">Size</h6> <label class="radio"> <input type="radio"
-                                name="size" value="S" checked> <span>S</span> </label> <label
-                            class="radio">
-                            <input type="radio" name="size" value="M"> <span>M</span> </label> <label
-                            class="radio"> <input type="radio" name="size" value="L"> <span>L</span>
-                        </label> <label class="radio"> <input type="radio" name="size" value="XL">
-                            <span>XL</span> </label> <label class="radio"> <input type="radio"
-                                name="size" value="XXL"> <span>XXL</span> </label>
                     </div>
                     <div class="cart mt-4 align-items-center"> <button
-                            class="btn btn-danger text-uppercase mr-2 px-4">Add to cart</button> <i
+                            class="btn btn-danger text-uppercase mr-2 px-4" id="cart-btn">Add to cart</button> <i
                             class="bi bi-heart-fill text-muted"></i> <i
                             class="bi bi-share-fill text-muted"></i>
                     </div>
                 </div>
             </div>`
         displayProductImages(data.images);
+        generateSizeHTML(data.size);
+        addToCart(data.id);
     })
     .catch(error => {
         console.error('Error:', error);
     });
 
-    
+
 function displayProductImages(products) {
     var productImagesContainer = document.getElementById("product-images");
-  
+
     var mainImage = document.createElement("img");
     mainImage.id = "main-image";
     mainImage.width = 250;
@@ -66,20 +56,85 @@ function displayProductImages(products) {
     var mainImageContainer = document.createElement("div");
     mainImageContainer.className = "text-center p-4";
     mainImageContainer.appendChild(mainImage);
-  
+
     var thumbnailContainer = document.createElement("div");
     thumbnailContainer.className = "thumbnail text-center";
     for (var i = 0; i < products.length; i++) {
-      var thumbnailImage = document.createElement("img");
-      thumbnailImage.width = 70;
-      thumbnailImage.className = "m-2"
-      thumbnailImage.src = products[i].file;
-      thumbnailImage.onclick = function() {
-        mainImage.src = this.src;
-      };
-      thumbnailContainer.appendChild(thumbnailImage);
+        var thumbnailImage = document.createElement("img");
+        thumbnailImage.width = 70;
+        thumbnailImage.className = "m-2"
+        thumbnailImage.src = products[i].file;
+        thumbnailImage.onclick = function () {
+            mainImage.src = this.src;
+        };
+        thumbnailContainer.appendChild(thumbnailImage);
     }
     productImagesContainer.appendChild(mainImageContainer);
     productImagesContainer.appendChild(thumbnailContainer);
-  }
-  
+}
+
+function addToCart(productId) {
+    const addToCartBtn = document.getElementById("cart-btn");
+    if (!addToCartBtn) return;
+
+    const sizeInputs = document.querySelectorAll('input[name="size"]');
+    let selectedSize;
+
+    sizeInputs.forEach(input => {
+        input.addEventListener("change", event => {
+            selectedSize = event.target.value;
+        });
+    });
+
+    addToCartBtn.addEventListener("click", event => {
+        const jwt = localStorage.getItem("jwt");
+        const products = productId;
+
+        if (!selectedSize) {
+            alert("Please select a size");
+            return;
+        }
+        if (!jwt) {
+            alert("Login to add this product to cart!");
+            return;
+        }
+
+        fetch(`${apiUrl}/cart/items/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwt}`
+            },
+            body: JSON.stringify({
+                products,
+                size: selectedSize,
+            })
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                alert("Product added to cart");
+            })
+            .catch(error => {
+                console.error(error);
+                alert("Failed to add product to cart");
+            });
+    });
+}
+
+function generateSizeHTML(array) {
+    array.sort((a, b) => a.size - b.size);
+    let html = `<h6 class="text-uppercase">Size</h6>`;
+    array.forEach(size => {
+        html += `<label class="radio me-1">
+                    <input type="radio" name="size" value="${size.size}" ${size.checked ? 'checked' : ''}>
+                    <span>${size.size}</span>
+                </label>`;
+    });
+    document.querySelector('.sizes.mt-5').innerHTML = html;
+}
